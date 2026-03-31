d8f5c84c034eeb75bf697fabd9c8be24bced93bc
dastan <dastanjunushaliev@gmail.com>
2026-03-31 11:30:10 +0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
+function showPage(page) {
+    document.getElementById('home-page').style.display = page === 'home' ? 'block' : 'none';
+    document.getElementById('empty-page').style.display = page === 'empty' ? 'block' : 'none';
+}
 
+function openModal(name) {
+    document.getElementById('itemName').innerText = name;
+    document.getElementById('orderModal').style.display = 'block';
+}
+
+function closeModal() {
+    document.getElementById('orderModal').style.display = 'none';
+}
+
+function confirmPurchase() {
+    alert("Заказ принят!");
+    closeModal();
+}
